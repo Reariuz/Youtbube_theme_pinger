21971301a4b14c8b07bfe63aa5613b01a8712196
Patrik Garten <info@garten.dev>
2024-03-08 20:26:04 +0100

    Remove unused dependencies and code optimize

--- main.py
+++ main.py
@@ -1,145 +1,95 @@
 import json
 import math
-import pprint
 
 from youtube_transcript_api import YouTubeTranscriptApi
-from youtube_api import get_current_video,get_video_metadata
+from youtube_api import get_current_video, get_video_metadata
 
-result_list =[]
-search_list = []
+def read_file_to_list(filename, delimiter=';'):
+    """Reads a file into a list of lines."""
+    try:
+        with open(filename, 'r') as fp:
+            return [line.strip().split(delimiter) for line in fp]
+    except FileNotFoundError:
+        print(f"File {filename} not found.")
+        return []
 
+def search(word, transcript):
+    """Searches the transcript for the specified word."""
+    return [line for line in transcript if word in line['text']]
 
-# searches for a term of the searchlist in the transcript returns the line
-def search(term,transcript):
-    result_list.clear()
-    for line in transcript:
-        if term in line['text']:
-            result_list.append(line)
-    
-    return result_list
+def get_transcript(video_id, language='de'):
+    """Gets the transcript for a video in the specified language."""
+    try:
+        return YouTubeTranscriptApi.get_transcript(video_id, languages=[language])
+    except Exception as e:
+        print(f"Error getting transcript for video {video_id}: {e}")
+        return []
 
+def calculate_relevance(result, weight):
+    """Calculates the relevance of a search result."""
+    return math.log(len(result) + 1) * int(weight)
 
+def format_result(word, results, weight):
+    """Formats the search results for a word."""
+    message = [f'Das Wort "{word}" kommt {len(results)} mal vor und ist mit {weight} gewichtet.']
+    for result in results:
+        minutes = int(result['start'] / 60)
+        seconds = int(result['start'] % 60)
+        text = result['text'].replace("\n", " ")
+        message.append(f"+ {minutes}:{seconds:02d} - {text} <br>")
+    message.append("---")
+    return message
+
+def generate_output(video, overall_weight, found_words, resulting_table):
+    """Generates and prints the output message based on the search results."""
+    video_metadata = get_video_metadata(video['latest_Video_Id'])
+    video_url = f"https://youtu.be/{video['latest_Video_Id']}?feature=shared"
+    message = [
+        f"[**{video_metadata}**]({video_url})",
+        video['Channel_Name'],
+        "---",
+        f"*Relevanz: **{overall_weight:.2f}***<br>",
+        "*Folgende Wörter wurden im Video identifiziert:*<br>"
+    ] + [f"+{word}<br>" for word in found_words] + ["---"] + resulting_table
+
+    filename = f"{video['Channel_Name']}_testfile.md"
+    with open(filename, 'w') as f:
+        for line in message:
+            f.write(f"{line}\n")
+    print(f"Output written to {filename}")
 
 def main():
+    channel_list = read_file_to_list('Channel_list.txt')
+    search_terms = read_file_to_list('search_terms.txt')
+    
+    try:
+        with open('current_videos.json', 'r') as fp:
+            previous_videos = json.load(fp)
+    except FileNotFoundError:
+        print("current_videos.json not found, proceeding without previous videos.")
+        previous_videos = []
 
-    Channel_list = []
-    video_list = []
-    control_list = []
+    video_list = [get_current_video(channel_id, channel_name) for channel_id, channel_name in channel_list]
+    new_videos = [video for video in video_list if video not in previous_videos]
 
-    # Open the Channel List and transfer to Channel_list
-    with open('Channel_list.txt', 'r') as file:
-        for line in file:
-            # Read all the lines of the file into a list
-            Channel = line.replace("\n","").split(';')
-            Channel_list.append(Channel)
-
-    # Open the search term List and transfer to Search_list
-    with open('search_terms.txt', 'r') as file:
-        for line in file:
-            # Read all the lines of the file into a list
-            term = line.replace("\n","").split(';')
-            search_list.append(term)
-
-
-    # get the current video from youtube_api function
-    for line in Channel_list:
-        video_list.append(get_current_video(line[0],line[1]))
-
-
-    #load a list of the last checked videos an channels to compare
-    with open('current_videos.json', 'r') as fp:
-        previous_video = json.load(fp)
-
-
-    #Compares the lists to generate the Control_list containing only the changed entrys
-    for line in video_list:
-        if not line in previous_video:
-            control_list.append(line)
-
-    '''
-    ### TODO - de-comment this paragraph to write the new video_ids to file for next-time comparission
-    ### DONOT de-comment this before because it will overwrite the json-file and future test might not create videos
-    erst datei schreiben wenn programm fertig
+    # Save the updated list of videos for future comparisons
     with open('current_videos.json', 'w') as fp:
         json.dump(video_list, fp)
-    '''
 
-    #main loop
+    for video in new_videos:
+        transcript = get_transcript(video['latest_Video_Id'])
+        overall_weight, found_words, resulting_table = 0, [], []
 
-    for line in control_list:
-        relevance_indicator = bool(False)
+        for word, weight in search_terms:
+            result = search(word, transcript)
+            relevance = calculate_relevance(result, int(weight))
+            if result:
+                overall_weight += relevance
+                found_words.append(word)
+                resulting_table.extend(format_result(word, result, weight))
 
-        #get the transcript from the video in german
-        transcript = YouTubeTranscriptApi.get_transcript(line['latest_Video_Id'], languages=['de'])
-
-        #set the relevancy value for each video back to 0
-        overall_weight = 0
-
-        resulting_table = []
-        found_words = []
-
-
-        for word in search_list:
-
-            result = search(word[0],transcript)
-
-
-            if math.log(len(result)+1)*int(word[1]) > 0:
-                relevance_indicator = bool(True)
-                new_list =[]
-
-                found_words.append(word[0])
-
-                new_list.append('Das Wort:"'+ word[0] +'" kommt '+ str(len(result))+ ' mal vor und ist mit '+ str(word[1])+ ' gewichtet')
-
-                #adding the weight to the overall using logaryhmic
-                overall_weight += math.log(len(result)+1)*int(word[1])
-
-                #reformats the transcript results
-                for line2 in result:
-                    new_dict ={} 
-                    new_dict['text']=str(line2['text']).replace("\n", " ")
-                    new_dict['minutes'] = int(line2['start']/60)
-                    new_dict['seconds'] = int(line2['start']%60)
-                    new_list.append(new_dict)
-                resulting_table.append(new_list)
-
-
-        #if Words were found in video toggle the formating 
-        if relevance_indicator == bool(True):
-            #Format the results line-wise and write them to a list
-            print("create message for:" + str(line))
-            message = []
-            message.append("[**"+get_video_metadata(line['latest_Video_Id'])+"**](https://youtu.be/"+line['latest_Video_Id']+"?feature=shared)")
-            message.append(line['Channel_Name'])
-            message.append("---")
-            message.append("*Relevanz: **"+str(overall_weight)+"***"+"<br>")
-            message.append("*Folgende Wörter wurden im Video identifiziert:*"+"<br>")
-            #adding all the words from the searchlist that has ben found
-            for word in found_words:
-                message.append("+"+word+"<br>")
-            #format and add all the wordspecific timestamps and quotes    
-            message.append("---")
-            for line in resulting_table:
-                message.append(line[0])
-                for subline in line[1:]:
-                    ### TODO -keep try except until final tests
-                    try:
-                        message.append("+ " + str(subline['minutes'])  + ":" + str(subline['seconds'])+ " - " +str(subline['text']))
-                    except:
-                        print("error")
-                        print(subline)
-                message.append("---")
-
-            pprint.pprint(message)
-
-            print("write file")
-            with open(message[1]+'_testfile.md', 'w') as f:
-                for row in message:
-                    f.write(f"{row}\n")
-            message.clear()  
-    
-    #pprint.pprint(message)
+        if found_words:
+            generate_output(video, overall_weight, found_words, resulting_table)
 
 if __name__ == "__main__":
     main()
